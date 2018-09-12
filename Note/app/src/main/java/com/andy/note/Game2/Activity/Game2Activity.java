e0c42b320ff8711c00e5f3a0d1c54f22b91fc9c5
AndyLau907 <907838289@qq.com>
2018-09-12 17:13:35 +0800

    添加贪吃蛇的相关文件

---
+++ Note/app/src/main/java/com/andy/note/Game2/Activity/Game2Activity.java
@@ -0,0 +1,22 @@
+package com.andy.note.Game2.Activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.andy.note.R;
+
+/**
+ * Created by andy on 2018/9/12.
+ */
+
+public class Game2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game2);
+
+
+    }
+}
---
+++ Note/app/src/main/java/com/andy/note/Game2/Views/Game2View.java
@@ -0,0 +1,26 @@
+package com.andy.note.Game2.Views;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by andy on 2018/9/12.
+ */
+
+public class Game2View extends View {
+    public Game2View(Context context) {
+        this(context, null);
+    }
+
+    public Game2View(Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public Game2View(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+
+}
